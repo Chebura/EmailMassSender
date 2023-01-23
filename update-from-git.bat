794dfab66ffda600565a9e62e66b061351967914
Kalianov Dmitry <kalianovda@ekassir.com>
2023-01-23 17:36:38 +0300

    fix

--- update-from-git.bat
+++ update-from-git.bat
@@ -1,8 +1,9 @@
 REM install git for windows
 rename ".\EmailMassSender\usersettings.json" ".\EmailMassSender\usersettings.~son"
-ren ".\EmailMassSender\appsettings.json" ".\EmailMassSender\appsettings.~son"
+rename ".\EmailMassSender\appsettings.json" ".\EmailMassSender\appsettings.~son"
 git pull -f
 del ".\EmailMassSender\usersettings.json"
 del ".\EmailMassSender\appsettings.json"
-ren ".\EmailMassSender\usersettings.~son" ".\EmailMassSender\usersettings.json"
-ren ".\EmailMassSender\appsettings.~son" ".\EmailMassSender\appsettings.json"
+rename ".\EmailMassSender\usersettings.~son" ".\EmailMassSender\usersettings.json"
+rename ".\EmailMassSender\appsettings.~son" ".\EmailMassSender\appsettings.json"
+dotnet build
