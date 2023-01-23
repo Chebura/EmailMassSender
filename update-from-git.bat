07de8cdb920003c196e95bd65af262183e520ad3
Kalianov Dmitry <kalianovda@ekassir.com>
2023-01-23 17:29:03 +0300

    fix

---
+++ update-from-git.bat
@@ -0,0 +1,8 @@
+REM install git for windows
+ren ./EmailMassSender/usersettings.json ./EmailMassSender/usersettings.~son
+ren ./EmailMassSender/appsettings.json ./EmailMassSender/appsettings.~son
+git pull -f
+del ./EmailMassSender/usersettings.json
+del ./EmailMassSender/appsettings.json
+ren ./EmailMassSender/usersettings.~son ./EmailMassSender/usersettings.json
+ren ./EmailMassSender/appsettings.~son ./EmailMassSender/appsettings.json
